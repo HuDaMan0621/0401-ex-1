4e8967c1374209eb44ba344de6fdde884554ee7d
Ying Xuan Hu <hudaman@Yings-MacBook-Pro.local>
2020-04-01 11:02:21 -0400

    added more notes to exercise.py

--- exercise.py
+++ exercise.py
@@ -1,1 +1,14 @@
 hello world
+
+#step 0: Document the problem
+#add a couple of numbers and print it out
+
+#1. Setup/Configuration
+result = 0 #give it a sane default
+num_1 = 10
+num_2 = 11
+
+#3. Do the work 
+result = num_1 + num_2
+
+#4
